ceacde069bfab3344d027f9c920689fb7c812678
Alberto <mib.albert@gmail.com>
2022-11-26 09:40:10 +0200

    finished the cart

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,20 +1,23 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 
-const Context = createContext()
+const Context = createContext();
 
 export const StateContext = ({ children }) => {
-  const [showCart, setShowCart] = useState(false)
-  const [cartItems, setCartItems] = useState([])
-  const [totalPrice, setTotalPrice] = useState(0)
-  const [totalQuantities, setTotalQuantities] = useState(0)
-  const [qty, setQty] = useState(1)
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
+  const [qty, setQty] = useState(1);
+
+  let foundProduct;
+  let index;
 
   const onAdd = (product, quantity) => {
-    const checkProductInCart = cartItems.find((item) => item._id === product._id )
+    const checkProductInCart = cartItems.find((item) => item._id === product._id);
     
-    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
-    setTotalQuantities((prevTotalQuatities) => prevTotalQuatities + quantity)
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
     
     if(checkProductInCart) {
       const updatedCartItems = cartItems.map((cartProduct) => {
@@ -23,28 +26,54 @@ export const StateContext = ({ children }) => {
           quantity: cartProduct.quantity + quantity
         }
       })
-      setCartItems(updatedCartItems)
-    
+
+      setCartItems(updatedCartItems);
     } else {
-      product.quantity = quantity
-      setCartItems([...cartItems, {...product}])
+      product.quantity = quantity;
+      
+      setCartItems([...cartItems, { ...product }]);
     }
-    toast.success(`${qty} ${product.name} added to the cart.`)
+
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  } 
+
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id);
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice -foundProduct.price * foundProduct.quantity);
+    setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity);
+    setCartItems(newCartItems);
   }
 
-  const toggleCartItemQuantity = (id, value) => {
+  const toggleCartItemQuanitity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id)
+    index = cartItems.findIndex((product) => product._id === id);
+    const newCartItems = cartItems.filter((item) => item._id !== id)
 
+    if(value === 'inc') {
+      setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+      setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+    } else if(value === 'dec') {
+      if (foundProduct.quantity > 1) {
+        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+      }
+    }
   }
 
   const incQty = () => {
-    setQty((prevQty) => prevQty + 1 )
+    setQty((prevQty) => prevQty + 1);
   }
 
   const decQty = () => {
     setQty((prevQty) => {
-      if(prevQty - 1 < 1) return 1
-      return prevQty - 1 
-    })
+      if(prevQty - 1 < 1) return 1;
+     
+      return prevQty - 1;
+    });
   }
 
   return (
@@ -58,7 +87,12 @@ export const StateContext = ({ children }) => {
         qty,
         incQty,
         decQty,
-        onAdd
+        onAdd,
+        toggleCartItemQuanitity,
+        onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities 
       }}
     >
       {children}
@@ -66,5 +100,4 @@ export const StateContext = ({ children }) => {
   )
 }
 
-export const useStateContext = () => useContext(Context)
-
+export const useStateContext = () => useContext(Context);
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Toaster } from 'react-hot-toast'
+import NextNProgress from 'nextjs-progressbar'
 
 import '../styles/globals.css'
 
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
+        <NextNProgress color='red' height={5}/>
         <Toaster />
         <Component {...pageProps} />
       </Layout>
