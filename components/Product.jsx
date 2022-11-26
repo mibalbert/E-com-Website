f822647feddac8d38fb03f7f88fe42665ccbbf7e
Alberto <mib.albert@gmail.com>
2022-11-26 03:03:52 +0200

    working on the individual product coponent

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Product = () => {
+import { urlFor } from '../lib/client'
+
+const Product = ({ product: { image, name, slug, price} }) => {
   return (
-    <div>Product</div>
+    <div>
+      {console.log(product)}
+          <p>asdasdasd</p>
+      <Link href={`/product/${slug.current}`}>
+        <div className='produc-card'>
+          <img 
+            src={urlFor(image && image[0])}
+            width={250} 
+            height={250}
+            className='product-image' />
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,43 @@
 import React from 'react'
 
+import { client } from '../lib/client'
 import { Product, FooterBanner, HeroBanner } from '../components'
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
       <div className='products-heading'>
         <h2>Best Selling Products!</h2>
         <p>Speaker of many variations</p>
       </div>
 
-
+      <p>asdasdasd</p>
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map( (product) => {
-          product
+        {/* {products ? console.log(products) : console.log("some wrong")} */}
+        {products?.map( (product) => {
+          <Product key={product._id} product={product}/>
         })}
       </div>
 
-      Footer
+      <FooterBanner />
 
     </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
 
 export default Home
